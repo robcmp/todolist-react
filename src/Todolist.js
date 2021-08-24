7aeaaaea307105be0ff1a8372d729aa03e888347
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-23 23:10:10 -0400

    added functions to be used in input/button events

--- src/Todolist.js
+++ src/Todolist.js
@@ -4,12 +4,23 @@ const TodoList = () => {
     let [addTask, setTask] = useState('');
     let [addList, setList] = useState([]);
 
+    const addingToDo = (ev) => {
+        setList([...addList, addTask]);
+        setTask("");
+    }
+
+    const valueChange = (e) => {
+        setTask(e.target.value);
+    }
+
     return (
         <div>
             <div className="input-group mb-3">
                 <input type="text" value={addTask} className="form-control" placeholder="Add to Do" aria-label="Add to Do" aria-describedby="button-addon2" onChange="" />
                 <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick="">Button</button>
             </div>
+
+
 
         </div >
 
