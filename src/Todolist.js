849cdd9d2e706a5fe1ae776d05e05e5355d79198
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-23 23:23:34 -0400

    added function and ul html to add task to list

--- src/Todolist.js
+++ src/Todolist.js
@@ -16,11 +16,15 @@ const TodoList = () => {
     return (
         <div>
             <div className="input-group mb-3">
-                <input type="text" value={addTask} className="form-control" placeholder="Add to Do" aria-label="Add to Do" aria-describedby="button-addon2" onChange="" />
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick="">Button</button>
+                <input type="text" value={addTask} className="form-control" placeholder="Add to Do" aria-label="Add to Do" aria-describedby="button-addon2" onChange={valueChange} />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={addingToDo}>Button</button>
             </div>
 
-
+            <ul>
+                {
+                    addList.map((li, key) => <li key={key} >{li}</li>)
+                }
+            </ul>
 
         </div >
 
