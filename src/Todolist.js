7e9339774cd5a727d0334db36e3c3f64e2dba2b9
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-23 23:09:06 -0400

    added input/button to add task in todolist

--- src/Todolist.js
+++ src/Todolist.js
@@ -4,7 +4,16 @@ const TodoList = () => {
     let [addTask, setTask] = useState('');
     let [addList, setList] = useState([]);
 
+    return (
+        <div>
+            <div className="input-group mb-3">
+                <input type="text" value={addTask} className="form-control" placeholder="Add to Do" aria-label="Add to Do" aria-describedby="button-addon2" onChange="" />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick="">Button</button>
+            </div>
 
+        </div >
+
+    );
 }
 
 export default TodoList;
