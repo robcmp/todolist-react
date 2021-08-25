5be1b724a58f1773072651fe58544ad48078ab94
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-25 16:42:39 -0400

    added component TodoList.js with correct name file

---
+++ src/TodoList.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+
+const TodoList = () => {
+    let [addTask, setTask] = useState('');
+    let [addList, setList] = useState([]);
+
+    const addingToDo = (ev) => {
+        setList([...addList, addTask]);
+        setTask("");
+    }
+
+    const valueChange = (e) => {
+        setTask(e.target.value);
+    }
+
+    const deleteTask = (key) => {
+        setList(addList.filter((item, index) => index !== key));
+    }
+
+    return (
+        <div className="d-flex justify-content-center">
+            <div class="card" >
+                <div class="card-body">
+                    <h5 class="card-title">Todo List</h5>
+                    <div>
+                        <div className="input-group mb-3">
+                            <input type="text" value={addTask} className="form-control" placeholder="Add to Do" aria-label="Add to Do" aria-describedby="button-addon2" onChange={valueChange} />
+                            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={addingToDo}>Add Task</button>
+                        </div>
+                        <ul className="list-group">
+                            {
+                                addList.map((li, key) => <li className="list-group-item list-group-item-info" key={key} >{li}<span className="ps-5"><button onClick={() => {
+                                    deleteTask(key);
+                                }}><i class="bi bi-x"></i></button></span></li>)
+                            }
+                        </ul>
+                    </div >
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default TodoList;
