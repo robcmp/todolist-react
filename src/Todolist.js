af15ded70064a7b804bcce5f4f6ec333016b8d57
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-24 23:15:33 -0400

    added html to look the design better

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+
 const TodoList = () => {
     let [addTask, setTask] = useState('');
     let [addList, setList] = useState([]);
@@ -18,21 +19,29 @@ const TodoList = () => {
     }
 
     return (
-        <div>
-            <div className="input-group mb-3">
-                <input type="text" value={addTask} className="form-control" placeholder="Add to Do" aria-label="Add to Do" aria-describedby="button-addon2" onChange={valueChange} />
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={addingToDo}>Button</button>
+        <div className="d-flex justify-content-center">
+            <div class="card" >
+
+                <div class="card-body">
+                    <h5 class="card-title">Todo List</h5>
+                    <div>
+                        <div className="input-group mb-3">
+                            <input type="text" value={addTask} className="form-control" placeholder="Add to Do" aria-label="Add to Do" aria-describedby="button-addon2" onChange={valueChange} />
+                            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={addingToDo}>Add Task</button>
+                        </div>
+
+                        <ul className="list-group">
+                            {
+                                addList.map((li, key) => <li className="list-group-item list-group-item-info" key={key} >{li}<span className="ps-5"><button onClick={() => {
+                                    deleteTask(key);
+                                }}><i class="bi bi-x"></i></button></span></li>)
+                            }
+                        </ul>
+
+                    </div >
+                </div>
             </div>
-
-            <ul>
-                {
-                    addList.map((li, key) => <li key={key} onClick={() => {
-                        deleteTask(key);
-                    }}>{li}</li>)
-                }
-            </ul>
-
-        </div >
+        </div>
 
     );
 }
